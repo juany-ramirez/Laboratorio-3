5acfb7601842c1bc02f5475542ed52384c9d0e3c
juany-ramirez <juany.ramrz@unitec.edu>
2016-02-05 17:31:36 -0600

    Arreglos

--- Laboratorio3.cpp
+++ Laboratorio3.cpp
@@ -7,53 +7,67 @@ using std::endl;
 using std::cin;
 
 int menor(int []);
+int posicionMayor;
 
 int main(int argc, char*argv[]){
-        
-        int partida=1 ,menu, primera=0;
-        while(partida==1){
+        const int size = 10;
+        int data [size];
+        int data2 [size];
+	int partida=1 ,menu, primera=0;
+        while(partida==	1){
 		cout << "Ingresar opcion a menu: "<< endl <<"1. Random adivinar numero. "<< endl << "2. Opcion 2 " << endl;
-		int menu;	
-                srand (time(NULL));
-                int magic = -500 + rand()%((500+1) -(-500)), currentTry;
-                cout<< "---"<<magic<<"---";
-                bool hasWon = false;
-		int contador=0;
-               	while(hasWon == false ){
-                        cout << "Intento # "<<contador<< ": ";
-                        cin >> currentTry; 
-                        if(currentTry == magic){
-                                cout<< "Numero adivinado" << endl;
-                                hasWon = true;
-                                break;
-                        }
-			contador++;
-               }
-		const int size = 10; 
-	      	int data [size];
-		if(primera==0)
+		cin >> menu;
+		if(menu == 1){	
+                	srand (time(NULL));
+                	int magic = -500 + rand()%((500+1) -(-500)), currentTry;
+                	cout<< "---"<<magic<<"---";
+                	bool hasWon = false;
+			int contador=0;
+        		while(hasWon == false ){
+        			cout << "Intento # "<<contador<< ": ";
+       	        	       	cin >> currentTry; 
+       	        		if(currentTry == magic){
+       	              			 cout<< "Numero adivinado" << endl;
+       	                		hasWon = true;
+	               			break;
+	         		}
+				contador++;
+        		 }
+			if(primera==0){
 			for(int i=0;i<size ; i++)
-				data[i] =100000;
-	        int data2 [size];
-		for(int i=0; i<size ; i++)
-			data2 [i] = data [i];  
-		for(int i=0; i<size; i++){
-			data[i] = menor(data2);
-			int k=0;
-			while(!data[i]==data2[k]){
-				k++;		
+					data[i] =100000;
+			primera=1;
 			}
-			data2[k]=100000;
-		}
-		for(int i=0; i<size ; i++){
-			if(data[i]!=100000){
-				cout << data[i] << endl;	
+			int p=-1;
+				for(int i=size-1; i<=0 ; i++){
+				if(i==100000)
+					p=i;
 			}
-		}	
-	        cout<< "Desea salir: "<< endl << "1. No"<<endl <<"2. Si"; 
-	        cin>> partida;
-	        cout << endl;
- 
+			if(p!=-1){
+				data[p] = contador; 	
+			}else if(data[9]>contador){
+				data[9] = contador;
+			}
+			for(int i=0; i<size ; i++)
+				data2 [i] = data [i];  
+			for(int i=0; i<size; i++){
+				data[i] = menor(data2);
+				int k=0;
+				while(!data[i]==data2[k]){
+					k++;		
+				}
+				data2[k]=100000;
+			}
+			cout << "High Scores"<< endl;
+			for(int i=0; i<size ; i++){
+				if(data[i]!=100000){
+					cout << data[i] << endl;	
+				}
+			}	
+		        cout<< "Desea salir: "<< endl << "1. No"<<endl <<"2. Si"; 
+		        cin>> partida;
+		        cout << endl;
+ 		}
 	}
         return 0;
 }
