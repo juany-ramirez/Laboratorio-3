f09a07b9b004721d16ac4f1ff5db9d5a49882eae
juany-ramirez <juany.ramrz@unitec.edu>
2016-02-05 17:00:02 -0600

    Funciones con el arreglo

--- Laboratorio3.cpp
+++ Laboratorio3.cpp
@@ -1,4 +1,4 @@
-include <iostream>
+#include <iostream>
 #include <ctime>
 #include <cstdlib>
 
@@ -6,17 +6,18 @@ using std::cout;
 using std::endl;
 using std::cin;
 
+int menor(int []);
+
 int main(int argc, char*argv[]){
-        char partida = 's';
-        int rango, intentos;
-        while((partida == 's') || (partida =='S')){
+        
+        int partida=1 ,menu, primera=0;
+        while(partida==1){
+		cout << "Ingresar opcion a menu: "<< endl <<"1. Random adivinar numero. "<< endl << "2. Opcion 2 " << endl;
+		int menu;	
                 srand (time(NULL));
-                int magic = rand()% 500+(-500), currentTry;
+                int magic = -500 + rand()%((500+1) -(-500)), currentTry;
                 cout<< "---"<<magic<<"---";
-                cout << "Ingrese el numero de intentos deseados: ";
-                cin >> intentos;
                 bool hasWon = false;
-                double tercio, diferencia;
 		int contador=0;
                	while(hasWon == false ){
                         cout << "Intento # "<<contador<< ": ";
@@ -28,28 +29,44 @@ int main(int argc, char*argv[]){
                         }
 			contador++;
                }
-	}
-	const int size = 10; 
-        double data [size];
-        for(int i=0; i<size ; i++){
-		if(data [size] == NULL){
-			
+		const int size = 10; 
+	      	int data [size];
+		if(primera==0)
+			for(int i=0;i<size ; i++)
+				data[i] =100000;
+	        int data2 [size];
+		for(int i=0; i<size ; i++)
+			data2 [i] = data [i];  
+		for(int i=0; i<size; i++){
+			data[i] = menor(data2);
+			int k=0;
+			while(!data[i]==data2[k]){
+				k++;		
+			}
+			data2[k]=100000;
+		}
+		for(int i=0; i<size ; i++){
+			if(data[i]!=100000){
+				cout << data[i] << endl;	
+			}
 		}	
+	        cout<< "Desea salir: "<< endl << "1. No"<<endl <<"2. Si"; 
+	        cin>> partida;
+	        cout << endl;
+ 
 	}
-        for(int i=0; i<size ; i+i){
-       		cout << "Numero ["<< i << "]= ";
-        	cin >> data[i];
-        }
-	
-        cout<< "Desea salir"<< endl; 
-        cin>> partida;
-        cout << endl;
-        
         return 0;
 }
 
 
-
+int menor(int data []){
+	int menor=10000000;
+ 	for(int i=0;i<10 ;i++){
+		if(data[i]<menor)
+			menor = data[i];
+	}
+	return menor;
+}
 
 
 
