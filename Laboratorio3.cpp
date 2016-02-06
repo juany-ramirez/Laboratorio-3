bdd0d74e602f14d9a5b6594c6a2be3ba9bc9bd2a
juany-ramirez <juany.ramrz@unitec.edu>
2016-02-05 19:27:45 -0600

    Fin (acabo de llegar y este teclado basura me arruino todoT_T)

--- Laboratorio3.cpp
+++ Laboratorio3.cpp
@@ -7,7 +7,9 @@ using std::endl;
 using std::cin;
 
 int menor(int []);
-int posicionMayor;
+int rand1();
+int rand2();
+int rand3();
 
 int main(int argc, char*argv[]){
         const int size = 10;
@@ -15,15 +17,15 @@ int main(int argc, char*argv[]){
         int data2 [size];
 	int partida=1 ,menu, primera=0;
         while(partida==	1){
-		cout << "Ingresar opcion a menu: "<< endl <<"1. Random adivinar numero. "<< endl << "2. Opcion 2 " << endl;
+						cout << "Ingresar opcion a menu: "<< endl <<"1. Random adivinar numero. "<< endl << "2. Opcion 2 " << endl;
 		cin >> menu;
 		if(menu == 1){	
                 	srand (time(NULL));
                 	int magic = -500 + rand()%((500+1) -(-500)), currentTry;
                 	cout<< "---"<<magic<<"---";
-                	bool hasWon = false;
-			int contador=0;
-        		while(hasWon == false ){
+        		bool hasWon=true;
+			int contador=1;
+			while(hasWon == false ){
         			cout << "Intento # "<<contador<< ": ";
        	        	       	cin >> currentTry; 
        	        		if(currentTry == magic){
@@ -31,26 +33,34 @@ int main(int argc, char*argv[]){
        	                		hasWon = true;
 	               			break;
 	         		}
+				if(currentTry<magic){
+					cout << "El numero es menor" << endl; 
+				}
+				if(currentTry>magic){
+					cout << "El numero es mayor"<< endl;
+				}
 				contador++;
-        		 }
+        		}
 			if(primera==0){
 			for(int i=0;i<size ; i++)
 					data[i] =100000;
 			primera=1;
 			}
-			int p=-1;
-				for(int i=size-1; i<=0 ; i++){
-				if(i==100000)
+			int p = 10;
+			for(int i = size-1; 0 <= i ; i--){
+				if(data [i]==100000)
 					p=i;
 			}
-			if(p!=-1){
+			cout <<"****************"<< p<< endl;
+			if(!p==10){
 				data[p] = contador; 	
 			}else if(data[9]>contador){
 				data[9] = contador;
 			}
 			for(int i=0; i<size ; i++)
 				data2 [i] = data [i];  
-			for(int i=0; i<size; i++){
+		
+			for(int i=0; i<p; i++){
 				data[i] = menor(data2);
 				int k=0;
 				while(!data[i]==data2[k]){
@@ -68,6 +78,56 @@ int main(int argc, char*argv[]){
 		        cin>> partida;
 		        cout << endl;
  		}
+		if(menu==2){
+			int row =4;
+			int column =3;
+			int matriz[row][column];
+			int q=0;
+			for(int i=0; i<row ; i++){	
+				for(int k=0; k<column ; k++){
+					if(k==1)
+						matriz[i][k] = rand1();
+					if(k==2)
+						matriz[i][k] = rand2();
+					if(k==3)
+						matriz[i][k] = rand3();	
+				}
+			}
+			for(int i=0; i<row ; i++){      
+                                for(int k=0; k<column ; k++){
+                                 	cout << matriz[i][k] << "\t"; 	      
+                                }
+				cout << endl;
+                        }
+
+			int r1= rand()%3;
+			int r2= rand()%3;
+			int r3= rand()%3;
+			int r4= rand()%3;
+			int r5= rand()%3;
+			int r6= rand()%3;
+			int a1, a2,v1,v2,d1,d2;
+			int punto1,punto2;
+			a1 = matriz[r1][0];
+			a2 = matriz[r2][0];
+			d1=matriz[r3][1];
+			d2=matriz[r4][1];
+			v1=matriz[r5][2];
+			v2=matriz[r6][2];
+			punto1= a1-d2;
+			punto2= a2-d1;
+			
+			if(punto1==punto2){
+				if(v1>v2){
+					cout<<"Jugador1 GANADOR";
+				}else
+					cout<<"Jugador2 GANADOR";
+			}else if(punto1<punto2){
+				cout<<"Jugador 2 GANADOR";
+			}else{
+                                cout<<"Jugador 1 GANADOR";
+                        }			
+		}
 	}
         return 0;
 }
@@ -80,6 +140,21 @@ int menor(int data []){
 			menor = data[i];
 	}
 	return menor;
+}
+
+int rand1 (){
+	int magic = 85 + rand()%((115+1) -(85));
+	return magic;
+}
+
+int rand2 (){
+        int magic = 150 + rand()%((200+1) -(150));
+	return magic;
+}
+
+int rand3 (){
+        int magic = 150 + rand()%((150+1) -(200));
+	return magic;
 }
 
 
